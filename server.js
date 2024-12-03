7fe32ff581a1bbf53bf185db448bcffe69aa3eae
utkarsh-choudhary <90688693+utkarsh-choudhary@users.noreply.github.com>
2024-12-03 11:47:20 +0530

    cors

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.listen(PORT, () =>
 
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN, 
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true
 }));
